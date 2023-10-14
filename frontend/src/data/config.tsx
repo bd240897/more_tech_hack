972a3153677f49d9fdf1cc7f4baf9043f3c26e10
bd240897 <bd240897@yandex.ru>
2023-10-15 00:23:29 +0300

    fix(react): react config.tsx, correct name of var .env, fix Dockerfile for frontend - copy package.json first

--- frontend/src/data/config.tsx
+++ frontend/src/data/config.tsx
@@ -10,8 +10,6 @@ const configDefault = {
     urls: {
         backURL: 'localhost',
         backPort: '1111',
-        wsURL: 'localhost',
-        wsPort: '1111',
     }
 }
 
@@ -25,13 +23,15 @@ const getBoolEnv = (data: string | undefined): boolean | null => {
 
 const configEnv = {
     logging: {
-        isConsoleLogs: getBoolEnv(import.meta.env.VITE_BACK_HOST_CONSOLE_LOG),
+        isConsoleLogs: getBoolEnv(import.meta.env.VITE_CONSOLE_LOG),
     },
     urls: {
-        backURL: import.meta.env.VITE_BACK_HOST ?? null,
+        backURL: import.meta.env.VITE_BACK_URL ?? null,
         backPort: import.meta.env.VITE_BACK_PORT ?? null,
     }
 }
+
+console.log("configEnv", configEnv)
 
 const config = mergeWith({}, configDefault, configEnv,
     (a: any, b: any) => b === null ? a : undefined
